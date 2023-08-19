0cc3d2873ca4d210ab90b412f30950fdc3538ad2
EhabBellkasy <EhabBellkasy@gmail.com>
2023-08-20 00:39:22 +0300

    start test sampling data

--- GetChart/YellowCandel.py
+++ GetChart/YellowCandel.py
@@ -6,11 +6,9 @@ from plotly.subplots import make_subplots
 
 def fun (dataFrame) :
 
-        
-
         #Make Yellow Color Candel 
         dataFrame['MCOverrides'] = [None]*len(dataFrame)
-        # print(dataFrame.head(3))
+
         for ts in dataFrame.index:
             if    ( (dataFrame.loc[ts,'Open']    ==  dataFrame.loc[ts,'Close']) and 
                     (dataFrame.loc[ts,'Volume']  >   0)):
@@ -20,84 +18,8 @@ def fun (dataFrame) :
                     (dataFrame.loc[ts,'High']    == dataFrame.loc[ts,'Low']) and 
                     (dataFrame.loc[ts,'Volume']  == 0)):
                                                     dataFrame.loc[ts,'MCOverrides'] = '#8A8A8A'
-        # print(dataFrame.head(3))
-
-        # print("/*-/*-/*-/*-")
+        
         mco = dataFrame['MCOverrides'].values
-        # print(len(mco))
-        # print((mco))
-
-
-        # for ei in range (len(mco)):
-        #         if    (mco[ei] == '#FCFC00'):
-        #                mco[ei] = mc_Y
-        #         elif  (mco[ei] == '#8A8A8A'):
-        #                mco[ei] = mc_G
-        # mco[0] = mc_Y
-        # mco[1] = mc_Y
-        # mco[2] = mc_Y
-        # mco[3] = mc_Y
-        # mco[4] = mc_Y
-        # mco[5] = mc_Y
-        # mco[6] = mc_Y
-        # mco[7] = mc_Y
-        # mco[8] = mc_Y
-        # mco[9] = mc_Y
-
-        # print("/*-/*-/*-/*-")
-        # print((mco))
-        '''
-
-
-
-
-        #marketcolors
-        mc = mpf.make_marketcolors(up='g',
-                                down='r',
-                                edge='inherit',
-                                wick={'up':'w','down':'w'},
-                                volume='in',
-                                ohlc='i'
-                                
-                                
-                                )
-
-        #make mpf style
-        styleEhab  = mpf.make_mpf_style(marketcolors=mc,
-                                        gridaxis= 'both',
-                                        gridstyle= 'dashdot',
-                                        gridcolor= '#ffff00', #'yellow'
-                                        figcolor = 'gray',
-                                        facecolor= 'Black',
-                                        edgecolor= 'r',
-                                        
-                                        y_on_right= False
-
-                                        
-                                        )
-
-        '''
-
-
-        # #Make Yallow Color Candel 
-        # dataFrame['MCOverrides'] = [None]*len(dataFrame)
-        # print(dataFrame.head(3))
-        # for ts in dataFrame.index:
-        #     if    ( (dataFrame.loc[ts,'Open']    ==  dataFrame.loc[ts,'Close']) and 
-        #             (dataFrame.loc[ts,'Volume']  >   0)):
-        #                                             dataFrame.loc[ts,'MCOverrides'] = '#FCFC00' # Yellow
-        #     elif  ( (dataFrame.loc[ts,'Open']    == dataFrame.loc[ts,'Close']) and 
-        #             (dataFrame.loc[ts,'Open']    == dataFrame.loc[ts,'High']) and 
-        #             (dataFrame.loc[ts,'High']    == dataFrame.loc[ts,'Low']) and 
-        #             (dataFrame.loc[ts,'Volume']  == 0)):
-        #                                             dataFrame.loc[ts,'MCOverrides'] = '#8A8A8A' # Grey
-        # print(dataFrame.head(3))
-        # mco = dataFrame['MCOverrides'].values
-
-
-
-
-
         return mco
 
 
--- GetChart/v06_fristTry.py
+++ GetChart/v06_fristTry.py
@@ -25,28 +25,35 @@ import TimeStamp as TS
 # Set Varibles
 #------------------------------------------------     
 filePath = r'D:\Python Tools\ChartMaker\SourceDocuments\OutPut_Excel\gv mmm.xlsx'
-filePathChart = r'D:\Python Tools\ChartMaker\SourceDocuments\OutPut_jpg\Chart test.png' #    C:\Users\lenovo\Desktop\Python Project\Ehab\Results\Chart test.jpg
+filePathChart = r'D:\Python Tools\ChartMaker\SourceDocuments\OutPut_jpg\Chart test' #    C:\Users\lenovo\Desktop\Python Project\Ehab\Results\Chart test.jpg
+imageType= '.png'
+bookName = "Yahoo 1m"
+timeScope = "1m"
+captureDay= "2023-08-03 "
 
+index = 0
+
+# Loop 1 
 # Import  data
-df = ImportData.fun (filePath_fun = filePath , bookName = "Yahoo 1m")
+dft = ImportData.fun (filePath_fun = filePath , bookName = bookName)
 
+# Loop 2
 # Sampling data
-# dft = df[TS.index1_1m_A[0]:TS.index2_1m_A[0]]
-dft = df["2023-08-03 09:50:00":"2023-08-03 11:51:00"]
+df = dft[(captureDay + TS.index1_1m_A[index]):(captureDay + TS.index2_1m_A[index])]
 
 #Make Yellow Color Candel 
-mco = YellowCandel.fun(dataFrame = dft) 
+mco = YellowCandel.fun(dataFrame = df) 
 
 #Make a fibonacci lines
 fabalines = FibonacciLines.fun (filePath_fun = filePath)
 
 #Make Indicator
-EMA = addIndicator.fun(dataFrame = dft, scope = "1m" )
+EMA = addIndicator.fun(dataFrame = df, scope = timeScope )
 
 #Saving plot to a file
 # Link: https://github.com/matplotlib/mplfinance/blob/master/examples/savefig.ipynb
 #------------------------------------------------
-save = dict(fname= filePathChart, dpi= 500, pad_inches= 0)
+save = dict(fname= (filePathChart + '_' + (captureDay + TS.index3_1m_A[index]) + imageType), dpi= 500, pad_inches= 0)
 
 
 
@@ -55,7 +62,7 @@ save = dict(fname= filePathChart, dpi= 500, pad_inches= 0)
 #------------------------------------------------
 #Make a chart
 #------------------------------------------------
-mpf.plot(data                   = dft,
+mpf.plot(data                   = df,
          title                  = '\n test Chart', 
          type                   = 'candle', 
          #mav                    = (20,50),
@@ -67,7 +74,7 @@ mpf.plot(data                   = dft,
          scale_padding          = 1.0,
          #figsize               = (30,10),
          #ylim                   = (((df.Low.min())*0.95) ,((df.High.max())*1.05)), # set min and max of Chart
-         ylim                   = (((dft.Low.min())-0.03) ,((dft.High.max())+0.03)), # set min and max of Chart
+         ylim                   = (((df.Low.min())-0.03) ,((df.High.max())+0.03)), # set min and max of Chart
          xrotation              = 0,
          yscale                 = "linear", # y-axis scale: "linear", "log", "symlog", or "logit"
          volume_yscale          = "linear", # Volume y-axis scale: "linear", "log", "symlog", or "logit"
